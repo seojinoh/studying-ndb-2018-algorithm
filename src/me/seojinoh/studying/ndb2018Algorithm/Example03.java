6c7c8ec34bd419673721e584328b7ccd651d7bf6
Seojin Oh <seojinoh.me@gmail.com>
2022-03-09 13:18:59 +0900

    docs : Update Example03 description

--- src/me/seojinoh/studying/ndb2018Algorithm/Example03.java
+++ src/me/seojinoh/studying/ndb2018Algorithm/Example03.java
@@ -2,8 +2,8 @@ package me.seojinoh.studying.ndb2018Algorithm;
 
 // 삽입 정렬 (Insertion Sort)
 // 앞에서부터 뒤에 슬롯을 하나씩 열어가며 열린 슬롯의 뒤에서부터 가까이에 있는 두 수를 비교해서 더 큰 수를 뒤로 보내며 정렬하는 알고리즘으로써, 구현이 어렵고 비교적 느림
-// 위치 변경이 필요할 때에만 이루어지므로, 데이터가 이미 거의 정렬된 상태에 한해서는 가장 빠르다는 특징이 있음
 // 시간 복잡도: O(N^2)
+// 위치 변경이 필요할 때에만 이루어지므로, 데이터가 이미 거의 정렬된 상태에 한해서는 가장 빠르다는 특징이 있음
 public class Example03 {
 
 	public static void main(String[] args) {
